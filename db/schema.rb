ad2992b65e3f18571a6b9d778c8a719db4390957
swaption2009 <swaption2009@gmail.com>
2016-09-27 11:56:53 -0700

    feat: role-based access
    - install Pundit gem
    - migration add_column role in User table
    - create enum role: [:user, :author, :admin]
    - create method to set user’s default role to :user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927145825) do
+ActiveRecord::Schema.define(version: 20160927183906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160927145825) do
     t.datetime "updated_at",                          null: false
     t.string   "name",                   default: "", null: false
     t.string   "avatar"
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
