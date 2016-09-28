c3a74e43396fdacfc9613875e60b5afdc5621820
Ryan Collins <rccproductions@mac.com>
2016-09-28 15:33:56 -0400

    Docs: add documentation to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :user
+  # The default status is Draft, so the order needs
+    # to stay as it is for now.
+  enum status: [:draft, :published, :archived]
 
   def slug
     title.parameterize
   end
-  enum status: [:draft, :published, :archived]
 end
