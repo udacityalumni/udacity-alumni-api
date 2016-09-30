0a3810cf151b3d09d47ae04094fcccae230b33f2
Ryan Collins <rccproductions@mac.com>
2016-09-29 21:21:56 -0400

    Feat: update tests with new route namespacing

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
     it "works! (now write some real specs)" do
-      get articles_path
+      get api_v1_articles_path
       expect(response).to have_http_status(200)
     end
   end
