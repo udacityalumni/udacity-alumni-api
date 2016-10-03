ef6b5f048135481d01d978d5ea855f178bef1be4
swaption2009 <swaption2009@gmail.com>
2016-10-03 00:30:42 -0700

    update ffaker data for User and Article

--- spec/factories/article.rb
+++ spec/factories/article.rb
@@ -1,12 +1,13 @@
+require 'ffaker'
+
 FactoryGirl.define do
   factory :article do
     user
-    title FFaker::HealthcareIpsum.words.map(&:capitalize).join(' ')
-    featured true
-    spotlighted false
-    content FFaker::HealthcareIpsum.paragraph
-    feature_image FFaker::Avatar.image
-    status 'draft'
-#    slug 'hello-slug'
+    title           FFaker::HealthcareIpsum.words.map(&:capitalize).join(' ')
+    featured        true
+    spotlighted     false
+    content         FFaker::HealthcareIpsum.paragraph
+    feature_image   FFaker::Avatar.image
+    status          'draft'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,11 @@
+require 'ffaker'
+
 FactoryGirl.define do
-  factory :user do |u|
-    name {"test"}
-    email {|u| "#{u.name}_#{rand(1000).to_s}@test.com" }
-    password {'Password'}
+  factory :user do
+    name                  FFaker::Name.name
+    email                 FFaker::Internet.email
+    password              'password'
+    password_confirmation 'password'
+    confirmed_at          Date.today
   end
 end
