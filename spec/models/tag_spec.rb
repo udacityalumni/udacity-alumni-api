6c49200c6e8f15c4cbabe37496abf1319ae355ad
Abigail Mathews <abbymathews@gmail.com>
2016-10-10 04:17:20 +0000

    Feat: Tags: Add simple test for Tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'has expected columns with expected options' do
+    it { should have_db_column(:tag).of_type(:string).with_options(null: false) }
+  end
+  
 end
