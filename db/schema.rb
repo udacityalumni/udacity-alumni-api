855b8c9191392e04de03aa62ff67b70aa7a24f5c
Abigail Mathews <abbymathews@gmail.com>
2016-10-10 03:54:03 +0000

    Feat: Tags: Generate Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009233632) do
+ActiveRecord::Schema.define(version: 20161010033700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,39 @@ ActiveRecord::Schema.define(version: 20161009233632) do
     t.datetime "updated_at",                    null: false
     t.integer  "status",        default: 0,     null: false
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+  end
+
+  create_table "articles_nanodegrees", id: false, force: :cascade do |t|
+    t.integer "article_id",    null: false
+    t.integer "nanodegree_id", null: false
+  end
+
+  create_table "nanodegrees", force: :cascade do |t|
+    t.string   "name"
+    t.string   "abbrev"
+    t.integer  "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "nanodegrees_profiles", id: false, force: :cascade do |t|
+    t.integer "profile_id",    null: false
+    t.integer "nanodegree_id", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.text     "about"
+    t.string   "webpage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +83,5 @@ ActiveRecord::Schema.define(version: 20161009233632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, type: :controller do
+RSpec.describe Api::V1::SessionsController, type: :controller do
 
 end
