ca4b93f7656f04d381611898764ca775689a2f03
Ryan Collins <rccproductions@mac.com>
2016-10-16 11:37:28 -0400

    Feat: add controller tests for tags controller

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Api::V1::TagsController, type: :controller do
+
+  describe 'when GET #index' do
+    before(:each) do
+      4.times {
+        FactoryGirl.create :tag
+      }
+      get :index
+    end
+
+    it 'returns 4 tags from the database' do
+      expect(json_response[:tags].length).to eq(4)
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe 'when GET #show' do
+    before(:each) do
+      @tag = FactoryGirl.create :tag
+      get :show, id: @tag.id
+    end
+
+    it 'returns the selected tag as json' do
+      expect(json_response[:tag][:tag]).to eql @tag.tag
+    end
+    it { should respond_with 200 }
+  end
+
+  describe "when PATCH/PUT #update" do
+    before(:each) do
+
+    end
+  end
+
+  describe 'when DELETE #destroy' do
+
+  end
 
 end
