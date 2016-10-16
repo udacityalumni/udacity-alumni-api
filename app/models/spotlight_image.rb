ac0a760f46278532028b8ca47d21b9481e508f37
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-10-16 17:36:16 -0400

    Update spotlight_image.rb

--- app/models/spotlight_image.rb
+++ app/models/spotlight_image.rb
@@ -1,5 +1,4 @@
 class SpotlightImage < ApplicationRecord
-  #before_create :generate_auth_token!
   validates :url, presence: true
   # Each spotlight_image will belong to a user
   belongs_to :user
