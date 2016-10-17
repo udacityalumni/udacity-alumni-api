f2493665e37ebe3545952f04b3142fc0c8e3788a
Ryan Collins <rccproductions@mac.com>
2016-10-17 16:41:56 -0400

    Feat: update tag model to take unique values

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :articles
-  validates :tag, presence: true
+  validates :tag, presence: true, uniqueness: { case_sensitive: false }
 
   def slug
     tag.parameterize
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017175954) do
+ActiveRecord::Schema.define(version: 20161017203917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20161017175954) do
     t.string   "tag",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["tag"], name: "index_tags_on_tag", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade do |t|
