4735bdef545cd8b85f9a4c7f79a2ed402c255059
Ryan Collins <rccproductions@mac.com>
2016-10-17 00:46:25 -0400

    Feat: add slug to tag and update graphql types

--- app/graph/types/article_type.rb
+++ app/graph/types/article_type.rb
@@ -10,6 +10,7 @@ ArticleType = GraphQL::ObjectType.define do
   field :feature_image, types.String, 'The featured image for the article'
   field :created_at, types.String, 'The date the article was created'
   field :updated_at, types.String, 'The date the article was updated'
-  field :featured, types.Boolean, 'The featured status of the article'
+  field :spotlighted, types.Boolean, 'Whether the article is spotlighted'
+  field :featured, types.Boolean, 'Whether the article is featured'
   field :tags, -> { types[TagType] }, 'Tags associated with the article'
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :articles
   validates :tag, presence: true
+
+  def slug
+    tag.parameterize
+  end
 end
