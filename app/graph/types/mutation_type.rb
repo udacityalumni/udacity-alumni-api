54e5266432aa3ca885bab2371879400c4098bf7c
Ryan Collins <rccproductions@mac.com>
2016-10-18 11:57:09 -0400

    Feat: add article update mutations

--- app/graph/types/mutation_type.rb
+++ app/graph/types/mutation_type.rb
@@ -3,4 +3,6 @@ MutationType = GraphQL::ObjectType.define do
 
   field :CreateTag, field: TagMutations::Create.field
   field :SaveAll, field: TagMutations::SaveAll.field
+
+  field :UpdateArticle, field: ArticleMutations::Update.field
 end
