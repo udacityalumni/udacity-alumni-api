89efe91ef537da2ab3c9128ae3cf37bf37a6670e
Ryan Collins <rccproductions@mac.com>
2016-10-19 15:05:07 -0400

    Feat: add article mutations for crud operations

--- app/graph/mutations/article_mutations.rb
+++ app/graph/mutations/article_mutations.rb
@@ -1,4 +1,37 @@
 module ArticleMutations
+  Create = GraphQL::Relay::Mutation.define do
+    name 'CreateArticle'
+    input_field :auth_token, !types.String
+    input_field :article, ArticleInputType
+
+    return_field :article, ArticleType
+
+    resolve -> (inputs, ctx) do
+      user = User.find_by(auth_token: inputs[:auth_token])
+      article_inputs = inputs[:article].to_h
+      if user
+        article = Article.create(
+          title: article_inputs.title,
+          content: article_inputs.content,
+          json: article_inputs.json,
+          status: article_inputs.status,
+          spotlighted: article_inputs.spotlighted
+        )
+        article.user = user
+        article.feature_image = article_inputs[:feature_image] if article_inputs[:feature_image]
+        if article_inputs[:tags]
+          article_inputs[:tags].to_a.each do |tag|
+            article.tags.find_or_create_by(tag: tag.tag)
+          end
+          article.tags.save!
+        end
+        article.save!
+        {
+          article: article
+        }
+      end
+    end
+  end
   Update = GraphQL::Relay::Mutation.define do
     name 'UpdateArticle'
     input_field :auth_token, !types.String
@@ -11,9 +44,40 @@ module ArticleMutations
       article_inputs = inputs[:article].to_h
       if user
         article = Article.find_by_id(inputs[:id])
-        article.update(article_inputs)
+        article.update(
+          title: article_inputs.title,
+          content: article_inputs.content,
+          json: article_inputs.json,
+          status: article_inputs.status,
+          spotlighted: article_inputs.spotlighted
+        )
+        article.user = user
+        article.feature_image = article_inputs[:feature_image] if article_inputs[:feature_image]
+        if article_inputs[:tags]
+          article_inputs[:tags].to_a.each do |tag|
+            article.tags.find_or_create_by(tag: tag.tag)
+          end
+          article.tags.save!
+        end
         article.save!
+        {
+          article: article
+        }
       end
+    end
+  end
+  Delete = GraphQL::Relay::Mutation.define do
+    name 'DeleteArticle'
+    input_field :auth_token, !types.String
+    input_field :id, !types.ID
+    return_field :deleted_id, types.ID
+    resolve -> (inputs, ctx) do
+      article = Article.find_by_id(inputs[:id])
+      id = article.id
+      article.destroy
+      {
+        deleted_id: id
+      }
     end
   end
 end
--- app/graph/types/mutation_type.rb
+++ app/graph/types/mutation_type.rb
@@ -4,7 +4,9 @@ MutationType = GraphQL::ObjectType.define do
   field :CreateTag, field: TagMutations::Create.field
   field :SaveAll, field: TagMutations::SaveAll.field
 
+  field :CreateArticle, field: ArticleMutations::Create.field
   field :UpdateArticle, field: ArticleMutations::Update.field
+  field :DeleteArticle, field: ArticleMutations::Delete.field
 
   field :CreateSpotlightImage, field: SpotlightImageMutations::Create.field
   field :UpdateSpotlightImage, field: SpotlightImageMutations::Update.field
