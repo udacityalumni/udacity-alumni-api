24bf1d7aa95f3bba169eca9d37c5dde58fa8ffca
Ryan Collins <rccproductions@mac.com>
2016-10-22 18:07:49 -0400

    Fix: user type ID vs Id

--- app/graph/types/user_type.rb
+++ app/graph/types/user_type.rb
@@ -1,7 +1,7 @@
 UserType = GraphQL::ObjectType.define do
   name 'User'
   description 'The application, non auth user type'
-  field :id, !types.Id, 'The id of the user'
+  field :id, !types.ID, 'The id of the user'
   field :public, !types.Boolean, 'Whether the user is public'
   field :name, !types.String, 'The name of the user'
   field :bio, types.String, 'The bio of the user'
