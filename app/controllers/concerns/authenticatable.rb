953433d935c390f7289ba744593502da5cc82f69
Ryan Collins <rccproductions@mac.com>
2016-11-09 15:01:37 -0500

    feat: Update the user password reset functionality

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -10,5 +10,4 @@ module Authenticatable
   def user_signed_in?
     current_user.present?
   end
-
 end
--- app/graph/mutations/user_mutations.rb
+++ app/graph/mutations/user_mutations.rb
@@ -19,4 +19,28 @@ module UserMutations
       end
     end
   end
+  ResetPassword = GraphQL::Relay::Mutation.define do
+    name 'ResetPassword'
+    description 'When a user returns to the password reset route with a token to reset password'
+    input_field :token, !types.String
+    input_field :password, !types.String
+    input_field :password_confirmation, !types.String
+
+    return_field :user, AuthUserType
+    resolve -> (inputs, _ctx) do
+      user = User.find_by(reset_password_token: inputs[:token])
+      if user && user.valid_password?(password)
+        user.update(
+          password: inputs[:password],
+          password_confirmation: inputs[:password_confirmation]
+        )
+        user.reset_password_token = nil
+        user.save!
+        sign_in_user! user
+        {
+          user: user
+        }
+      end
+    end
+  end
 end
