8c6fc453dc4a1e2be9fe4f8dddf8b7b110b4d15d
Ryan Collins <rccproductions@mac.com>
2016-11-18 14:36:33 -0500

    Feat: add created at for the user type

--- app/graph/types/user_type.rb
+++ app/graph/types/user_type.rb
@@ -6,4 +6,5 @@ UserType = GraphQL::ObjectType.define do
   field :name, !types.String, 'The name of the user'
   field :bio, types.String, 'The bio of the user'
   field :avatar, types.String, 'The avatar url of the user'
+  field :created_at, types.String, 'When the user was created'
 end
