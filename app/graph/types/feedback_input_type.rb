2f486fe43a53396051e1d920704f8ecf1729b65c
Chashmeet Singh <chashmeetsingh@gmail.com>
2016-12-12 01:05:55 +0530

    fix: feedback input type, feat: image migration

--- app/graph/types/feedback_input_type.rb
+++ app/graph/types/feedback_input_type.rb
@@ -3,5 +3,5 @@ FeedbackInputType = GraphQL::InputObjectType.define do
   description 'Feedback submitted by a user, input type'
   input_field :description, !types.String, 'Description of the feedback'
   input_field :url, types.String, 'The url where the feedback came from'
-  file_field :image, types.String, 'Image uploaded by the user'
+  input_field :image, types.String, 'Image uploaded by the user'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202100913) do
+ActiveRecord::Schema.define(version: 20161211192821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20161202100913) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.binary   "image"
     t.index ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
   end
 
